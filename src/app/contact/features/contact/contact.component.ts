03330e6914d54ea3eb4e3313c6740870f6171655
chakri ayoub <ayoub-chakri@hotmail.com>
2025-05-04 14:58:11 +0100

    FEATURE-7 / FEATURE-8 diaplay contact form with email & message fields

--- src/app/contact/features/contact/contact.component.ts
+++ src/app/contact/features/contact/contact.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    InputTextareaModule,
+    ButtonModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      email: [''],
+      message: ['']
+    });
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+
+      // TODO : afficher un message à l'utilisateur : "Demande de contact envoyée avec succès".
+     
+
+    } 
+  }
 
 }
